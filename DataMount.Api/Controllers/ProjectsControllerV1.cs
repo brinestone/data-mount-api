980e27047d5368f901f8347c335fc8d0f1397797
Conrad Bekondo <conradbekondo@gmail.com>
2026-05-21 10:33:42 +0100

    feat: add projects controller

---
+++ DataMount.Api/Controllers/ProjectsControllerV1.cs
@@ -0,0 +1,14 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataMount.Api.Controllers;
+
+[Authorize]
+[Tags("Project")]
+[ApiController]
+[ApiVersion("1.0")]
+[Route($"{Constants.ApiBasePath}/projects")]
+public class ProjectsControllerV1 : ControllerBase
+{
+}
